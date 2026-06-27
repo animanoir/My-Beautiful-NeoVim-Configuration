fe90c387e179d6612bebcbac3ef049501cf0e1a8
oscaramontiel <etherealflux94@gmail.com>
2026-06-27 13:36:22 -0600

    adds supercollider plugin

--- init.lua
+++ init.lua
@@ -66,6 +66,41 @@ vim.opt.rtp:prepend(lazypath)
 -- 3. PLUGINS
 --------------------------------------------------------------------------------
 require("lazy").setup({
+  -- SuperCollider
+  {
+    "davidgranstrom/scnvim",
+    ft = "supercollider",
+    config = function()
+      local scnvim = require("scnvim")
+      local map = scnvim.map
+      local map_expr = scnvim.map_expr
+
+      scnvim.setup({
+        keymaps = {
+          ["<M-e>"]      = map("editor.send_line", { "i", "n" }),
+          ["<C-e>"]      = {
+            map("editor.send_block", { "i", "n" }),
+            map("editor.send_selection", "x"),
+          },
+          ["<CR>"]       = map("postwin.toggle"),
+          ["<M-CR>"]     = map("postwin.toggle", "i"),
+          ["<M-L>"]      = map("postwin.clear", { "n", "i" }),
+          ["<C-k>"]      = map("signature.show", { "n", "i" }),
+          ["<F12>"]      = map("sclang.hard_stop", { "n", "x", "i" }),
+          ["<leader>st"] = map("sclang.start"),
+          ["<leader>sk"] = map("sclang.recompile"),
+          ["<F1>"]       = map_expr("s.boot"), -- arranca el servidor de audio
+          ["<F2>"]       = map_expr("s.meter"),
+        },
+        editor = {
+          highlight = { color = "IncSearch" },
+        },
+        postwin = {
+          float = { enable = false }, -- post window en split, no flotante
+        },
+      })
+    end,
+  },
   -- Neogit: plugin to see git graphs and stuff
   {
     "NeogitOrg/neogit",
